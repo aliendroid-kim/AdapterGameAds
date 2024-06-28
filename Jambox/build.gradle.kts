dce4973b4278b1da3c4b51373fc33638c5d655dc
reskin-aliendroid <rysasahrial@aliendro.id>
2024-06-28 21:26:55 +0700

    Titan-14

--- Jambox/build.gradle.kts
+++ Jambox/build.gradle.kts
@@ -42,20 +42,14 @@ dependencies {
     implementation ("androidx.appcompat:appcompat:1.4.1")
     implementation ("com.google.android.material:material:1.8.0")
     implementation ("androidx.multidex:multidex:2.0.1")
-    //implementation fileTree(include: ['*.jar', '*.aar'], dir: 'libs")
-    //Main Sdk Ads
     implementation ("com.google.android.gms:play-services-ads:23.1.0")
     implementation ("com.facebook.android:audience-network-sdk:6.17.0")
     implementation ("com.applovin:applovin-sdk:12.5.0")
     implementation ("com.startapp:inapp-sdk:5.0.0")
     implementation ("com.unity3d.ads:unity-ads:4.12.0")
-
-    //Applovin
     implementation ("com.applovin.mediation:facebook-adapter:6.17.0.0")
     implementation ("com.applovin.mediation:google-adapter:23.1.0.0")
     implementation ("com.applovin.mediation:unityads-adapter:4.12.0.0")
-
-    //Admob
     implementation ("com.google.ads.mediation:facebook:6.17.0.0")
     implementation ("com.google.ads.mediation:applovin:12.5.0.1")
     implementation ("com.google.ads.mediation:unity:4.12.0.0")
@@ -66,7 +60,7 @@ dependencies {
     implementation ("com.startapp:admob-mediation:2.9.1")
     implementation ("com.startapp:applovin-mediation:1.1.7")
     implementation ("com.github.ronaldwm2:alienads_propssdk:1.1.1")
-    implementation ("com.github.aliendroid-kim:SDKGames:1.0.10")
+    implementation ("com.github.aliendroid-kim:SDKGames:1.0.14")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation ("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
@@ -87,7 +81,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.aliendroid-kim"
                 artifactId = "AdapterGameAds"
-                version = "Titan-13"
+                version = "Titan-14"
 
                 from(components["release"])
 
